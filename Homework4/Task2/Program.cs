c9e25daa3c6bff2f056d6bec8ad7db79c102eda5
Natalia <natalia_terekhova@yahoo.com>
2023-01-27 13:47:25 +0300

    Added Task 2

---
+++ Homework4/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result= GetDigitSum (number);
+System.Console.WriteLine(result);
+
+int GetDigitSum(int num)
+{
+int count=0;
+int sum =0; 
+while (num>0)
+{
+    int num1=num%10;
+    sum = sum+num1;
+    num/=10;
+    count++;
+}
+return sum;
+}
+
+
+
+// static int GetCountNum(int num)
+// {
+// System.Console.WriteLine(num);
+// int count=0;
+//    while (num>0)
+//    {
+//    count++;
+//    num/=10;
+//    }
+//    return count;
+// }
