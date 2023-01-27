ddb1adc7db64cae655bcf3c63575d15f19559358
Natalia <natalia_terekhova@yahoo.com>
2023-01-27 13:24:51 +0300

    Added Task 1

---
+++ Homework4/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+System.Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+System.Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+int res = RiseToPower (a,b);
+System.Console.WriteLine(res);
+
+int RiseToPower (int num1, int num2)
+{
+     int power = Convert.ToInt32 (Math.Pow (num1, num2));
+
+return power;
+}
+
